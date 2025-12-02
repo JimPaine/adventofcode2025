2ab79da5f470261cd48c706ec7114a423a193b06
Jim paine <jim.paine@microsoft.com>
2025-12-02 16:37:02 +0000

    day2 part 2

--- days/day2.py
+++ days/day2.py
@@ -1,7 +1,7 @@
 import re
 
 
-def sum_invalid_ids(ids_path: str) -> int:
+def sum_invalid_ids(ids_path: str, part=1) -> int:
     ranges = []
     with open(ids_path, 'r') as file:
         ranges = file.readline().split(',')
@@ -11,8 +11,12 @@ def sum_invalid_ids(ids_path: str) -> int:
         min = int(r.split('-')[0])
         max = int(r.split('-')[1])
 
+        pattern = r'^(\d+)\1$'
+        if part == 2:
+            pattern = r'^(\d{1,})\1+$'
+
         for id in range(min, max + 1):
-            match = re.match(r'^(\d+)\1$', str(id))
+            match = re.match(pattern, str(id))
             if match:
                 invalid.append(id)
 
--- tests/test_day2.py
+++ tests/test_day2.py
@@ -4,11 +4,15 @@ from days.day2 import sum_invalid_ids
 
 def test_sum_invalid_ids_example():
     example_path = os.path.join(os.path.dirname(__file__), 'examples/day2')
-    invalid_sum = sum_invalid_ids(example_path)
+    invalid_sum = sum_invalid_ids(example_path, part=1)
     assert invalid_sum == 1227775554
+    invalid_sum = sum_invalid_ids(example_path, part=2)
+    assert invalid_sum == 4174379265
 
 
 def test_sum_invalid_ids_input():
     input_path = os.path.join(os.path.dirname(__file__), 'inputs/day2')
-    invalid_sum = sum_invalid_ids(input_path)
-    assert invalid_sum == 1015240600
+    invalid_sum = sum_invalid_ids(input_path, part=1)
+    assert invalid_sum == 34826702005
+    invalid_sum = sum_invalid_ids(input_path, part=2)
+    assert invalid_sum == 43287141963
