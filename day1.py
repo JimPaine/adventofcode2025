4995e50cc986dd29a6cab50c13695dd60dddda11
Jim paine <jim.paine@microsoft.com>
2025-12-03 14:45:14 +0000

    tweaks

--- day1.py
+++ day1.py
@@ -11,10 +11,7 @@ def decode_password(combination_path: str) -> tuple[int, int]:
 
     with open(combination_path, 'r') as file:
         for line in file:
-            dir = line[0]
-            moves = int(line[1:])
-            pos, clicks = rotate(pos, moves, dir, clicks)
-            logging.info(f"The dial is rotated {line} to point at {pos}.")
+            pos, clicks = rotate(pos, int(line[1:]), line[0], clicks)
             if pos == 0:
                 zeros += 1
 
@@ -23,10 +20,7 @@ def decode_password(combination_path: str) -> tuple[int, int]:
 
 def rotate(pos: int, moves: int, dir: str, clicks: int) -> tuple[int, int]:
     for _ in range(moves):
-        if dir == 'R':
-            pos = (pos + 1) % 100
-        else:
-            pos = (pos - 1) % 100
+        pos = (pos + 1) % 100 if dir == 'R' else (pos - 1) % 100
 
         if pos == 0:
             clicks += 1
