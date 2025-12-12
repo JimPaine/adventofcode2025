681b3ef3cbde9f0666d1b1d818bcbc02716813c8
Jim paine <jim.paine@microsoft.com>
2025-12-12 13:53:23 +0000

    day2 hack

--- day11.py
+++ day11.py
@@ -1,4 +1,6 @@
+
 import os
+import re
 import pytest
 import numpy as np
 
@@ -6,32 +8,36 @@ class Map:
     def __init__(self, raw):
         self.nodes = np.array([])
         self._parse(raw)
-
+        with open('test.out', 'w', encoding='UTF-8') as file:
+            file.write('# Test output\n')
 
     def walk(self, start, end) -> int:
-        if start == end:
-            return 1
-
-        node = [n for n in self.nodes if n.name == start][0]
-
-        paths = 0
-        for child in node.children:
-            paths += self.walk(child, end)
- 
+        s = [n for n in self.nodes if n.name == start][0]
+        paths = s.walk(end, start)
         return paths
 
 
     def _parse(self, raw):
+        # parents
         for row in raw:
             parts = row.split(' ')
 
             name = str(parts[0][:-1])
-            parent = self._try_add(name)
-
+            _ = self._try_get(name)
+        
+        # links
+        for row in raw:
+            parts = row.split(' ')
+            parent = self._try_get(parts[0][:-1])
             children = parts[1:]
-            parent.add_children(children)
+            nodes = []
+            for c in children:
+                nodes = np.append(nodes, [self._try_get(c)])
+  
+            parent.add_children(nodes)
 
-    def _try_add(self, name) -> object:
+
+    def _try_get(self, name) -> object:
         if not any(n.name == name for n in self.nodes):
             n = Node(name)
             self.nodes = np.append(self.nodes, [n])
@@ -45,8 +51,21 @@ class Node:
         self.children = children if children != None else []
 
 
-    def add_children(self, children: list[str]):
+    def add_children(self, children: list):
         self.children = np.append(self.children, children)
+
+
+    def walk(self, end, path: str) -> int:
+        if self.name == end:
+            with open('test.out', 'a', encoding='UTF-8') as file:
+                file.write(f'{path}\n')
+            return 1
+
+        exits = 0
+        for c in self.children:
+            exits += c.walk(end, f'{path},{c.name}')
+        return exits
+
 
 def _read(path: str) -> list:
     rows = []
@@ -77,5 +96,13 @@ def test_measure_part2(path: str, expected: int):
     absolute_path = os.path.join(os.path.dirname(__file__), path)
     raw = _read(absolute_path)
     map = Map(raw)
-    paths = map.walk('svr', 'out', ['dac', 'fft'])
-    assert paths == expected
+    _ = map.walk('svr', 'out')
+    count = 0
+    pattern = r'^.*(fft|dac).*(fft|dac).*$'
+    with open('test.out', 'r') as file:
+        for line in file:
+            match = re.match(pattern, line.rstrip('\n'))
+            if match:
+                count += 1
+
+    assert count == expected
