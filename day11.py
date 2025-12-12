1893df2b7ca88063716a6100aba9de26f8bf8e53
Jim paine <jim.paine@microsoft.com>
2025-12-12 07:07:41 +0000

    simpler part1

--- day11.py
+++ day11.py
@@ -9,26 +9,28 @@ class Map:
 
 
     def walk(self, start, end) -> int:
-        s = [n for n in self.nodes if n.name == start][0]
-        return s.walk(end)
+        if start == end:
+            return 1
+
+        node = [n for n in self.nodes if n.name == start][0]
+
+        paths = 0
+        for child in node.children:
+            paths += self.walk(child, end)
+ 
+        return paths
 
 
     def _parse(self, raw):
         for row in raw:
             parts = row.split(' ')
-            
-            children = parts[1:]
-            nodes = []
-            for c in children:
-                nodes = np.append(nodes, [self._try_add(c)])
- 
+
             name = str(parts[0][:-1])
             parent = self._try_add(name)
-            parent.add_children(nodes)
 
-            x = 1
+            children = parts[1:]
+            parent.add_children(children)
 
-    
     def _try_add(self, name) -> object:
         if not any(n.name == name for n in self.nodes):
             n = Node(name)
@@ -41,25 +43,10 @@ class Node:
     def __init__(self, name, children = None):
         self.name = name
         self.children = children if children != None else []
-        self.exits = -1
 
-    def add_children(self, children: list):
+
+    def add_children(self, children: list[str]):
         self.children = np.append(self.children, children)
-
-    def walk(self, end) -> int:
-        if self.name == end:
-            return 1
-
-        if self.exits != -1:
-            return self.exits
-
-        exits = 0
-        for c in self.children:
-            exits += c.walk(end)
-
-        self.exits = exits
-        return self.exits
-
 
 def _read(path: str) -> list:
     rows = []
@@ -70,13 +57,25 @@ def _read(path: str) -> list:
     return rows
 
 
-@pytest.mark.parametrize("path, part1", [
+@pytest.mark.parametrize("path, expected", [
     ("examples/day11", 5),
     ("inputs/day11", 652),
 ])
-def test_measure(path: str, part1):
+def test_measure_part1(path: str, expected: int):
     absolute_path = os.path.join(os.path.dirname(__file__), path)
     raw = _read(absolute_path)
     map = Map(raw)
-    count = map.walk('you', 'out')
-    assert count == part1
+    paths = map.walk('you', 'out')
+    assert paths == expected
+
+
+@pytest.mark.parametrize("path, expected", [
+    ("examples/day11_2", 2),
+    ("inputs/day11", 652),
+])
+def test_measure_part2(path: str, expected: int):
+    absolute_path = os.path.join(os.path.dirname(__file__), path)
+    raw = _read(absolute_path)
+    map = Map(raw)
+    paths = map.walk('svr', 'out', ['dac', 'fft'])
+    assert paths == expected
