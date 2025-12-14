2a09020b1d41ad9e6821c884dcbf2eeb060c79d4
Jim paine <jim.paine@microsoft.com>
2025-12-14 21:07:20 +0000

    part2

--- day11.py
+++ day11.py
@@ -2,7 +2,6 @@
 import os
 import pytest
 import numpy as np
-import asyncio
 
 class Map:
     def __init__(self, raw):
@@ -10,9 +9,12 @@ class Map:
         self._parse(raw)
 
 
-    async def walk(self, start, end, visited_dac = True, visited_fft = True) -> int:
+    def walk(self, start, end) -> int:
         s = [n for n in self.nodes if n.name == start][0]
-        return await s.walk(end, visited_dac, visited_fft)
+        for n in self.nodes:
+            n.exits = 0
+            n.visited = False
+        return s.walk(end)
 
 
     def _parse(self, raw):
@@ -48,62 +50,28 @@ class Node:
         self.name = name
         self.children = children if children != None else []
         self.exits = 0
-        self.parents = []
-        self.visits_dac = False
-        self.visits_fft = False
+        self.visited = False
     
 
     def add_children(self, children: list):
         self.children = np.append(self.children, children)
-        for c in children:
-            if len([p for p in c.parents if p.name == self.name]) == 0:
-                c.parents.append(self)
-            if c.name == 'dac':
-                c.set_dac()
-            elif c.name == 'fft':
-                c.set_fft()
-
-    def set_dac(self):
-        self.visits_dac = True
-        for p in self.parents:
-            p.set_dac()
 
 
-    def set_fft(self):
-        self.visits_fft = True
-        for p in self.parents:
-            p.set_fft()
-
-
-    async def walk(self, end, visited_dac = False, visited_fft = False) -> int:
-        if self.name == end and visited_dac and visited_fft:
+    def walk(self, end) -> int:
+        if self.name == end:
             return 1
-        elif self.name == end and end == 'dac':
-            return 1
-        elif self.name == end and end == 'fft':
-            return 1
-        elif self.name == end:
-            return 0
-
-        dac = True if self.name == 'dac' else visited_dac
-        fft = True if self.name == 'fft' else visited_fft
-        # if self.exits != 0 and visited_dac and visited_fft:
-        #     return self.exits
-        # else: 
-        #     self.exits = 0
-
-        tasks = []
-        for c in self.children:
-            if self.visits_dac and self.visits_fft:
-                tasks.append(c.walk(end, dac, fft))
-            elif visited_dac and visited_fft:
-                tasks.append(c.walk(end, dac, fft))
-            elif self.visits_dac and visited_fft:
-                tasks.append(c.walk(end, dac, fft))
-            elif visited_dac and self.visits_fft:
-                tasks.append(c.walk(end, dac, fft))
         
-        return np.sum(await asyncio.gather(*tasks))
+        if self.exits != 0:
+            return self.exits
+
+        for c in self.children:
+            if c.visited == False:
+                self.exits += c.walk(end)
+            else: 
+                self.exits += c.exits
+
+        self.visited = True 
+        return self.exits
 
 def _read(path: str) -> list:
     rows = []
@@ -114,34 +82,32 @@ def _read(path: str) -> list:
     return rows
 
 
-@pytest.mark.asyncio
 @pytest.mark.parametrize("path, expected", [
     ("examples/day11", 5),
     ("inputs/day11", 652),
 ])
-async def test_measure_part1(path: str, expected: int):
+def test_measure_part1(path: str, expected: int):
     absolute_path = os.path.join(os.path.dirname(__file__), path)
     raw = _read(absolute_path)
     map = Map(raw)
-    paths = await map.walk('you', 'out', True, True)
+    paths = map.walk('you', 'out', True, True)
     assert paths == expected
 
 
-@pytest.mark.asyncio
 @pytest.mark.parametrize("path, expected", [
     ("examples/day11_2", 2),
     ("inputs/day11", 652),
 ])
-async def test_measure_part2(path: str, expected: int):
+def test_measure_part2(path: str, expected: int):
     absolute_path = os.path.join(os.path.dirname(__file__), path)
     raw = _read(absolute_path)
     map = Map(raw)
-    # result = map.walk('svr', 'out', False, False) 
-    c = await map.walk('dac', 'out', True, True)
-    b = await map.walk('fft', 'dac')
-    a = await map.walk('svr', 'fft')
-    z = await map.walk('fft', 'out', True, True)
-    y = await map.walk('dac', 'fft') 
-    x = await map.walk('svr', 'dac')
+    c = map.walk('dac', 'out')
+    b = map.walk('fft', 'dac')
+    a = map.walk('svr', 'fft')
+    z = map.walk('fft', 'out')
+    y = map.walk('dac', 'fft') 
+    x = map.walk('svr', 'dac')
 
-    assert (a * b * c) + (x * y * z) == expected
+    result = (a * b * c) + (x * y * z)
+    assert result == expected
