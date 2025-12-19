f67866e258e0b1403764efc8b2d6a8614e2c60bc
Jim paine <jim.paine@microsoft.com>
2025-12-19 17:14:13 +0000

    needs a clean up

--- day10.py
+++ day10.py
@@ -9,7 +9,6 @@ class Machine:
 
     def __init__(self, raw: str):
         self._parse(raw)
-        return None
 
 
     def _parse(self, raw: str):
@@ -20,20 +19,30 @@ class Machine:
         raw_buttons = [s[1:-1].split(',') for s in temp[1:-1]]
         self._buttons = [Button(len(self._target_state), b) for b in raw_buttons]
 
+        self._free_variables = []
+        self._depends = []
+        self._build_augmented_matrix()
+
+
+    def _build_augmented_matrix(self):
+        x = len(self._buttons)
+
+        self._aug_matrix = [[0.0 for _ in range(x + 1)] for _ in range(len(self._target_joltage))]
+
+        for i, button in enumerate(self._buttons):
+            for j, switch in enumerate(button.flipped_switches):
+                self._aug_matrix[j][i] = float(switch)
+
+        for i, jolt in enumerate(self._target_joltage):
+            self._aug_matrix[i][x] = float(jolt)
+
 
     def _key(self, array: MachineState) -> str:
         return f'{array}'
 
-
     def initialize(self) -> int:
-        return self._walk(np.zeros(len(self._target_state), dtype='int'), self._target_state)
-
-
-    def configure_joltage(self) -> int:
-        return self._walk(np.zeros(len(self._target_joltage), dtype='int'), self._target_joltage, False)
-
-
-    def _walk(self, verticies: MachineState, target: MachineState, toggle=True) -> int:
+        verticies = np.zeros(len(self._target_state), dtype='int')
+        target = self._target_state
         queue = deque([verticies])
         start_key = self._key(verticies)
         visited = {start_key}
@@ -45,7 +54,7 @@ class Machine:
             current_key = self._key(current_state)
 
             for b in self._buttons:
-                next_state = b.click(current_state, toggle)
+                next_state = b.click(current_state)
                 next_key = self._key(next_state)
 
                 if next_key in visited:
@@ -60,25 +69,91 @@ class Machine:
                 queue.append(next_state)
         return 0
 
+    def configure_joltage(self) -> int:
+        self._gaussian_elimination()
+        return self._walk(0, np.zeros(len(self._free_variables), dtype=float))
+
+    def _check(self, values: list[float]) -> int:
+        total = sum(values)
+        for r in range(len(self._depends)):
+            v = self._aug_matrix[r][len(self._buttons)]
+            for i, c in enumerate(self._free_variables):
+                v -= self._aug_matrix[r][c] * values[i]
+
+            r = round(v)
+            if v < -1e-12 or abs(v - r) > 1e-12:
+               return None
+
+            total += int(r)
+        return total
+
+    def _walk(self, index: int, values, min=100000.0) -> int:
+        if index == len(self._free_variables):
+            r = self._check(values)
+            return r if r != None and r < min else min 
+
+        n = sum(values[:index])
+        for v in range(np.max(self._target_joltage)):
+            if n + v >= min:
+                break
+            values[index] = v
+            min = self._walk(index + 1, values, min)
+        values[index] = 0
+        return min
+
+    def _gaussian_elimination(self):
+        pivot = 0
+        cols = len(self._buttons)
+
+        for c in range(cols):
+            row = None
+            val = -1
+            for r in range(pivot, len(self._target_joltage)):
+                v = abs(self._aug_matrix[r][c])
+                if v > val:
+                    row = r
+                    val = v
+
+            if val < 1e-12:
+                self._free_variables.append(c)
+                continue
+
+            if row != pivot:
+                self._aug_matrix[pivot], self._aug_matrix[row] = self._aug_matrix[row], self._aug_matrix[pivot]
+            self._depends.append(c) 
+
+            v = self._aug_matrix[pivot][c]
+            for i in range(c, cols + 1):
+                self._aug_matrix[pivot][i] /= v
+
+            for r in range(len(self._target_joltage)):
+                if r == pivot:
+                    continue
+                factor = self._aug_matrix[r][c]
+                if abs(factor) > 1e-12:
+                    for j in range(c, cols + 1):
+                        self._aug_matrix[r][j] -= factor * self._aug_matrix[pivot][j]
+
+            pivot += 1
+
+        self._free_variables.extend([c for c in range(c, cols)])
+
 
 class Button:
 
     def __init__(self, size: int, raw: list[str]):
         self._configure_button(size, raw)
-        self.known_states = {}
 
 
     def _configure_button(self, size: int, switches: list[str]):
-        button = np.zeros(size, dtype='int')
+        flipped_switches = np.zeros(size, dtype='int')
         for s in switches:
-            button[int(s)] = 1
-        self._button = button
+            flipped_switches[int(s)] = 1
+        self.flipped_switches = flipped_switches
 
 
-    def click(self, current_state: MachineState, toggle: bool) -> MachineState:
-        if toggle:
-            return np.array(np.array(current_state, dtype='bool') ^ np.array(self._button, dtype='bool'), dtype='int')
-        return np.array(current_state, dtype='int') + np.array(self._button, dtype='int')
+    def click(self, current_state: MachineState) -> MachineState:
+        return np.array(np.array(current_state, dtype='bool') ^ np.array(self.flipped_switches, dtype='bool'), dtype='int')
 
 
 class Factory:
@@ -103,7 +178,7 @@ class Factory:
 
 @pytest.mark.parametrize("path, part1, part2", [
     ("examples/day10", 7, 33),
-    ("inputs/day10", 486, 0),
+    ("inputs/day10", 486, 17820),
 ])
 def test_initialize_machines(path: str, part1: int, part2: int):
     absolute_path = os.path.join(os.path.dirname(__file__), path)
